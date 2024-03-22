221a6ff00e6064d6b31f16986bb6054aff9fa39d
kaviyarasan <kaviyarasan.t@twilightitsolutions.com>
2024-03-22 12:09:23 +0530

    fix: format issues

--- app/LikeButton.js
+++ app/LikeButton.js
@@ -3,12 +3,12 @@
 import { useState } from 'react';
 
 export default function LikeButton() {
-    const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(0);
 
-    function handleClick() {
-      setLikes(likes + 1);
-    }
-    return (
-        <button onClick={handleClick}>Like ({likes})</button>
-    )
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return (
+    <button onClick={handleClick}>Like ({likes})</button>
+  )
 }
